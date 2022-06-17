6092b898c3dc530681ddb9c5b357bda720b61126
Kevin Castillo <kevincastillo318@gmail.com>
2022-06-17 12:38:11 -0700

    added styles and font

--- App.js
+++ App.js
@@ -2,13 +2,20 @@ import react from "react";
 import { StyleSheet, View, Text } from "react-native";
 import { globalStyles } from "./styles/global";
 import Navigator from './routes/homeStack'
+import { useFonts, YanoneKaffeesatz_400Regular, YanoneKaffeesatz_500Medium, YanoneKaffeesatz_700Bold } from '@expo-google-fonts/yanone-kaffeesatz'
+import AppLoading from 'expo-app-loading';
 
 export default function App() {
+  let [fontsLoaded] = useFonts({
+    YanoneKaffeesatz_400Regular, YanoneKaffeesatz_500Medium, YanoneKaffeesatz_700Bold
+  });
 
-
-  return (
-    <View style={globalStyles.container}>
-      <Navigator />
-    </View>
-  );
+    if(!fontsLoaded){
+      return <AppLoading />
+    }
+    return (
+      <View style={globalStyles.container}>
+        <Navigator />
+      </View>
+    )
 }
--- styles/global.js
+++ styles/global.js
@@ -3,7 +3,19 @@ import { StyleSheet } from "react-native";
 export const globalStyles = StyleSheet.create({
   container: {
     backgroundColor: "#f7f1e7",
-    padding: 24,
-    flex: 1
+    padding: 20,
+    flex: 1,
+    fontFamily: 'YanoneKaffeesatz_500Medium',
+    fontSize: 26
+  },
+  titleText: {
+    fontFamily: 'YanoneKaffeesatz_500Medium',
+    fontSize: 18,
+    backgroundColor: "#f7f1e7",
+  },
+  paragraph: {
+    marginVertical: 8,
+    lineHeight: 20,
+    backgroundColor: "#f7f1e7",
   }
 });
