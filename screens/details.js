363899434b5cf8fcb67a03676a45e7083280ab9f
Kevin Castillo <kevincastillo318@gmail.com>
2022-06-20 15:19:29 -0700

    added images to details, cards made

--- screens/details.js
+++ screens/details.js
@@ -1,13 +1,17 @@
 import React from "react";
-import { StyleSheet, View, Text } from "react-native";
+import { StyleSheet, View, Text, Image } from "react-native";
 import { globalStyles } from "../styles/global";
+import Card from "../shared/card";
 
 export default function Details({navigation}) {
     return (
         <View style={globalStyles.container}>
+            <Card>
             <Text style={globalStyles.titleDescription}>{navigation.getParam('title')}</Text>
             <Text style={globalStyles.titleDescription}>{navigation.getParam('subtitle')}</Text>
+            <Image source={navigation.getParam('src')}/>
             <Text style={globalStyles.titleDescription}>{navigation.getParam('price')}</Text>
+            </Card>
         </View>
     )
 }
--- screens/home.js
+++ screens/home.js
@@ -1,62 +1,72 @@
 import React, { useState } from "react";
-import { StyleSheet, View, Text, FlatList, TouchableOpacity, Image } from "react-native";
+import {
+  StyleSheet,
+  View,
+  Text,
+  FlatList,
+  TouchableOpacity,
+  Image,
+} from "react-native";
+import Card from "../shared/card";
 import { globalStyles } from "../styles/global";
 
 export default function Home({ navigation }) {
-    
-    const [featuredDrinks, setFeaturedDrinks] = useState([
-        {
-          title: "Carmella Latte",
-          subtitle: "A latte with a blend of two classic falvoers - caramel and vanilla",
-          price: "$5.00",
-          // color1: "#F9AC8A",
-          src: require("../assets/carmella_latte.png"),
-          aspectRatio: 1,
-          key: Math.random().toString() 
-        },
-      
-        {
-          title: "The Herbivore",
-          subtitle: "A latte with the perfect blend of lavender and honey",
-          price: "$5.00",
-          // color1: "#4DD2A5",
-          src: require("../assets/the_herbivore.png"),
-          aspectRatio: 1,
-          key: Math.random().toString()
-        },
-        {
-          title: "Bone Head",
-          subtitle: "Drip coffee witha double shot of espress",
-          price: "$5.00",
-          // color1: "#FEB829",
-          src: require("../assets/bonehead.png"),
-          aspectRatio: 1,
-          key: Math.random().toString()
-        },
-        {
-          title: "Nitro Cold Brew",
-          subtitle: "COMING SOON - cold brew with the addition of nitrogen gas to create a smooth texture",
-          price: "$5.00",
-          // color1: "#FE8E01",
-          src: require("../assets/nitro_cold_brew.png"),
-          aspectRatio: 1,
-          key: Math.random().toString()
-        }
-      ]);
+  const [featuredDrinks, setFeaturedDrinks] = useState([
+    {
+      title: "Carmella Latte",
+      subtitle:
+        "A latte with a blend of two classic falvoers - caramel and vanilla",
+      price: "$5.00",
+      // color1: "#F9AC8A",
+      src: require("../assets/carmella_latte.png"),
+      aspectRatio: 1,
+      key: Math.random().toString(),
+    },
 
-    return (
-            <View style={globalStyles.container}>
-               <FlatList 
-               data={featuredDrinks}
-               renderItem={({item}) => (
-                <TouchableOpacity onPress={() => navigation.navigate('Details', item)}>
-                    <Text style={globalStyles.titleText}>
-                        {item.title}
-                    </Text>
-                    <Image source={item.src}/>
-                </TouchableOpacity>
-               )}
-               />
-            </View>
-    )
+    {
+      title: "The Herbivore",
+      subtitle: "A latte with the perfect blend of lavender and honey",
+      price: "$5.00",
+      // color1: "#4DD2A5",
+      src: require("../assets/the_herbivore.png"),
+      aspectRatio: 1,
+      key: Math.random().toString(),
+    },
+    {
+      title: "Bone Head",
+      subtitle: "Drip coffee witha double shot of espress",
+      price: "$5.00",
+      // color1: "#FEB829",
+      src: require("../assets/bonehead.png"),
+      aspectRatio: 1,
+      key: Math.random().toString(),
+    },
+    {
+      title: "Nitro Cold Brew",
+      subtitle:
+        "COMING SOON - cold brew with the addition of nitrogen gas to create a smooth texture",
+      price: "$5.00",
+      // color1: "#FE8E01",
+      src: require("../assets/nitro_cold_brew.png"),
+      aspectRatio: 1,
+      key: Math.random().toString(),
+    },
+  ]);
+
+  return (
+    <View style={globalStyles.container}>
+      <FlatList
+        data={featuredDrinks}
+        renderItem={({ item }) => (
+          <TouchableOpacity
+            onPress={() => navigation.navigate("Details", item)}>
+            <Card>
+              <Text style={globalStyles.titleText}>{item.title}</Text>
+              <Image source={item.src} />
+            </Card>
+          </TouchableOpacity>
+        )}
+      />
+    </View>
+  );
 }
