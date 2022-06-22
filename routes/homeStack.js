dd106a2e9cb30c776faade4e39426479e56d1684
Kevin Castillo <kevincastillo318@gmail.com>
2022-06-22 16:29:16 -0700

    styling shopping cart component

--- routes/homeStack.js
+++ routes/homeStack.js
@@ -1,45 +1,18 @@
+import React from "react";
 import { createStackNavigator } from "@react-navigation/stack";
-//import { createStackNavigator } from "react-navigation-stack";
-import { createAppContainer } from "react-navigation";
 import { NavigationContainer } from "@react-navigation/native";
-import { globalStyles } from "../styles/global";
-import { StyleSheet } from "react-native";
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import Ionicons from "@expo/vector-icons";
+import FontAwesome  from '@expo/vector-icons';
 import Home from "../screens/home";
 import Details from "../screens/details";
+import Cart from "../screens/cart";
 
-// const screens = {
-//   Home: {
-//     screen: Home,
-//     navigationOptions: {
-//       title: "Fossil Fuel Coffee Co.",
-//     },
-//   },
-//   Details: {
-//     screen: Details,
-//     navigationOptions: {
-//       title: "Coffee Details",
-//     },
-//   },
-// };
-
-// const HomeStack = createStackNavigator(screens, {
-//   defaultNavigationOptions: {
-//     headerStyle: {
-//       backgroundColor: "#E64A33",
-//     },
-//     headerTitleStyle: {
-//       color: "white",
-//       textAlign: 'center',
-//     },
-//   },
-// });
-
-// export default createAppContainer(HomeStack);
-
+//home and details stack
 const HomeStack = createStackNavigator();
 const HomeStackScreen = () => (
-  <HomeStack.Navigator screenOptions={{ headerStyle: { backgroundColor: '#E64A33' }, headerTitleStyle: { color: 'white', textAlign: 'center'} }}>
-    <HomeStack.Screen name="Fossil Fuel Coffee Co." component={Home} options={({ route }) => {
+  <HomeStack.Navigator screenOptions={{ headerStyle: { backgroundColor: '#E64A33' }, headerTitleStyle: { color: 'white'} }}>
+    <HomeStack.Screen name="Fossil Fuel Coffee Co." component={Home} options={({}) => {
       return {
         headerTitleAlign: 'center'
       }
@@ -53,10 +26,30 @@ const HomeStackScreen = () => (
   </HomeStack.Navigator>
 );
 
+//cart stack for tab inclusion
+const CartStack = createStackNavigator();
+const CartStackScreen = () => (
+  <CartStack.Navigator screenOptions={{ headerStyle: { backgroundColor: '#E64A33' }, headerTitleStyle: { color: 'white'} }}>
+    <CartStack.Screen name="Shopping Cart" component={Cart} options={({}) => {
+      return {
+        headerTitleAlign: 'center'
+      }
+    }}/>
+  </CartStack.Navigator>
+)
 
+//tab screen output
+const AppTabs = createBottomTabNavigator();
+const AppTabsScreen = () => (
+  <AppTabs.Navigator screenOptions={{ headerShown: false, tabBarStyle: { backgroundColor: '#E64A33' } }} >
+    <AppTabs.Screen name="Home" component={HomeStackScreen} />
+    <AppTabs.Screen name="Cart" component={CartStackScreen} />
+  </AppTabs.Navigator>
+)
 
 export default () => (
   <NavigationContainer >
-    <HomeStackScreen />
+    {/* <HomeStackScreen /> */}
+    <AppTabsScreen />
   </NavigationContainer>
 );
