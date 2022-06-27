1f8be5c980d52060508589c29a37343fadce545b
Kevin Castillo <kevincastillo318@gmail.com>
2022-06-27 14:49:32 -0700

    scroll works on detials screen but card styling off

--- screens/details.js
+++ screens/details.js
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import { SafeAreaView, Text, Image, Button, StyleSheet } from "react-native";
+import { ScrollView, View, Text, Image, Button } from "react-native";
 import { globalStyles } from "../styles/global";
 import RadioForm, {
   RadioButton,
@@ -14,7 +14,7 @@ export default function Details({ navigation, route }) {
   const subtitle = route.params.subtitle;
   const price = route.params.price;
   const pic = route.params.src;
-  
+
   const [ice, setIce] = useState(false);
   const [checked, setChecked] = useState(false);
   var radio_props = [
@@ -23,47 +23,47 @@ export default function Details({ navigation, route }) {
   ];
 
   return (
-    <SafeAreaView style={globalStyles.container}>
-      <Card>
-        <Text style={globalStyles.drinkTitle}>{title}</Text>
-        <Image source={pic} />
-        <Text style={globalStyles.titleDescription}>{subtitle}</Text>
-        <Text style={globalStyles.titleDescription}>{price}</Text>
-        <Text style={globalStyles.titleDescription}>
-          Make it Iced
-          <RadioForm
-            radio_props={radio_props}
-            initial={-1}
-            formHorizontal={true}
-            selectedButtonColor={"#E64A33"}
-            buttonColor={"#E64A33"}
-            onPress={() => {
-              setIce(!ice);
-            }}
-          >
-          </RadioForm>
-        </Text>
-        <Text style={globalStyles.titleDescription}>
-          Extra Shot of Espresso - $1
-          <RadioForm
-            radio_props={radio_props}
-            initial={-1}
-            formHorizontal={true}
-            selectedButtonColor={"#E64A33"}
-            buttonColor={"#E64A33"}
-            onPress={() => {
-              setChecked(!checked);
-            }}
-          >
-          </RadioForm>
-        </Text>
-        <Button
-          style={globalStyles.cartButton}
-          title="Add to Cart"
-          color="#E64A33"
-          onPress={() => console.log(ice, checked)}
-        />
-      </Card>
-    </SafeAreaView>
+    <View style={globalStyles.container}>
+      <ScrollView>
+        <Card>
+          <Text style={globalStyles.drinkTitle}>{title}</Text>
+          <Image source={pic} />
+          <Text style={globalStyles.titleDescription}>{subtitle}</Text>
+          <Text style={globalStyles.titleDescription}>{price}</Text>
+          <Text style={globalStyles.titleDescription}>
+            Make it Iced
+            <RadioForm
+              radio_props={radio_props}
+              initial={-1}
+              formHorizontal={true}
+              selectedButtonColor={"#E64A33"}
+              buttonColor={"#E64A33"}
+              onPress={() => {
+                setIce(!ice);
+              }}
+            ></RadioForm>
+          </Text>
+          <Text style={globalStyles.titleDescription}>
+            Extra Shot of Espresso - $1
+            <RadioForm
+              radio_props={radio_props}
+              initial={-1}
+              formHorizontal={true}
+              selectedButtonColor={"#E64A33"}
+              buttonColor={"#E64A33"}
+              onPress={() => {
+                setChecked(!checked);
+              }}
+            ></RadioForm>
+          </Text>
+          <Button
+            style={globalStyles.cartButton}
+            title="Add to Cart"
+            color="#E64A33"
+            onPress={() => console.log(ice, checked)}
+          />
+        </Card>
+      </ScrollView>
+    </View>
   );
 }
--- shared/card.js
+++ shared/card.js
@@ -1,13 +1,13 @@
 import react from "react";
-import { StyleSheet, View } from "react-native";
+import { StyleSheet, ScrollView, View } from "react-native";
 
 export default function Card(props){
     return(
-        <View style={styles.card}>
+        <ScrollView style={styles.card}>
             <View style={styles.cardContent}>
                 { props.children }
             </View>
-        </View>
+        </ScrollView>
     )
 }
 
@@ -16,12 +16,13 @@ const styles = StyleSheet.create({
         borderRadius: 6,
         elevation: 3,
         backgroundColor: '#f7f1e7',
-        shadowOffset: {wdith:1, height:1},
+        shadowOffset: {width:1, height:1},
         shadowColor: "#333",
         shadowOpacity: 0.3,
         shadowRadius: 2,
         marginHorizontal: 4,
         marginVertical: 6,
+        marginBottom: 40,
     },
     cardContent: {
         marginHorizontal: 18,
